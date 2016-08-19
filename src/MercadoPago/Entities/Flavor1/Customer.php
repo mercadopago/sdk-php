21a5bdb67e2bf994bf0309db0911619b1b7f1e07
imasson <imasson@summasolutions.net>
2016-08-19 12:13:42 -0300

    Added payment entity save

---
+++ src/MercadoPago/Entities/Flavor1/Customer.php
@@ -0,0 +1,88 @@
+<?php
+namespace MercadoPago\Entities\Flavor1;
+
+use MercadoPago;
+use Doctrine\ORM\Mapping as ORM;
+use MercadoPago\Annotation\RestMethod;
+use MercadoPago\Annotation\RequestParam;
+use MercadoPago\Annotation\Attribute;
+
+/**
+ * @RestMethod(resource="/v1/customers/:id", method="read")
+ * @RestMethod(resource="/v1/customers/search", method="load")
+ * @RestMethod(resource="/v1/customers/", method="create")
+ * @RestMethod(resource="/v1/customers/:id", method="update")
+ * @RestMethod(resource="/v1/customers/:id", method="remove")
+ * @RequestParam(param="access_token")
+ */
+
+class Customer extends MercadoPago\Entity
+{
+    /**
+     * @Attribute(primaryKey = true)
+     */
+    protected $email;
+
+    /**
+     * @Attribute()
+     */
+    protected $id;
+    /**
+     * @Attribute()
+     */
+    protected $first_name;
+    /**
+     * @Attribute()
+     */
+    protected $last_name;
+    /**
+     * @Attribute()
+     */
+    protected $phone;
+    /**
+     * @Attribute()
+     */
+    protected $identification;
+    /**
+     * @Attribute()
+     */
+    protected $default_address;
+    /**
+     * @Attribute()
+     */
+    protected $address;
+    /**
+     * @Attribute()
+     */
+    protected $date_registered;
+    /**
+     * @Attribute()
+     */
+    protected $description;
+    /**
+     * @Attribute()
+     */
+    protected $date_created;
+    /**
+     * @Attribute()
+     */
+    protected $date_last_updated;
+    /**
+     * @Attribute()
+     */
+    protected $metadata;
+    /**
+     * @Attribute()
+     */
+    protected $default_card;
+    /**
+     * @Attribute()
+     */
+    protected $cards;
+    /**
+     * @Attribute()
+     */
+    protected $addresses;
+
+
+}
