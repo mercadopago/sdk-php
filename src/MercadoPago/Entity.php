621087459972d684c9d9ea629a68344d0034454d
feijaoatlive <feijao.costa@liveecommerce.com.br>
2020-04-01 17:36:27 -0300

    Ajuste no Entity para aceitar atributos como nulos

--- src/MercadoPago/Entity.php
+++ src/MercadoPago/Entity.php
@@ -470,21 +470,22 @@ abstract class Entity
      */
     protected function _fillFromArray($entity, $data)
     { 
-      
       if ($data) {
         
         foreach ($data as $key => $value) {
-            if (is_array($value)) {
-                $className = 'MercadoPago\\' . $this->_camelize($key);
-                if (class_exists($className, true)) {
-                    $entity->_setValue($key, new $className, false);
-                    $entity->_fillFromArray($this->{$key}, $value);
-                } else {
-                    $entity->_setValue($key, json_decode(json_encode($value)), false);
+            if (!is_null($value)){
+                if (is_array($value)) {
+                    $className = 'MercadoPago\\' . $this->_camelize($key);
+                    if (class_exists($className, true)) {
+                        $entity->_setValue($key, new $className, false);
+                        $entity->_fillFromArray($this->{$key}, $value);
+                    } else {
+                        $entity->_setValue($key, json_decode(json_encode($value)), false);
+                    }
+                    continue;
                 }
-                continue;
+                $entity->_setValue($key, $value, false);
             }
-            $entity->_setValue($key, $value, false);
         }
       }
     }
