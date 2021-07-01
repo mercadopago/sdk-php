951c2e972b6b66fcea49817c416e3fd44a5f33bd
Danilo Elias <danilo.elias@mercadolivre.com>
2021-07-01 10:18:39 -0300

    Fixing examples in samples folder

--- samples/index.php
+++ samples/index.php
@@ -2,7 +2,6 @@
 
     require_once 'vendor/autoload.php';
 
-    MercadoPago\SDK::setAccessToken("APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966");
-    // MercadoPago\SDK::setAccessToken("ACCESS_TOKEN");
+    MercadoPago\SDK::setAccessToken("ACCESS_TOKEN");
 
 ?>
