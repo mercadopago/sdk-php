a65d739d8853cd4f58f2c173b1a3f71d51d20dee
rhames07 <rhames.lima@mercadolivre.com>
2023-08-04 10:43:49 -0300

    Apply Property Promotion to MPResponse

--- src/MercadoPago/Net/MPResponse.php
+++ src/MercadoPago/Net/MPResponse.php
@@ -5,26 +5,30 @@ namespace MercadoPago\Net;
 /** MPResponse class. */
 class MPResponse
 {
-    private $status_code;
-
-    private $content;
-
     /**
      * MPResponse constructor.
      * @param int $status_code status code of the response.
      * @param mixed $content content of the response.
      */
-    public function __construct(int $status_code, $content)
-    {
-        $this->status_code = $status_code;
-        $this->content = $content;
+    public function __construct(
+        private int $status_code,
+        private mixed $content
+    ) {
     }
 
+    /**
+     * Get status code.
+     * @return int status code.
+     */
     public function getStatusCode(): int
     {
         return $this->status_code;
     }
 
+    /**
+     * Get content.
+     * @return array content.
+     */
     public function getContent(): array
     {
         return $this->content;
