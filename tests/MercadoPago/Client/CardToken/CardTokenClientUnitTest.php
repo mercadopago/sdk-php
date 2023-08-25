332b37be375d1cb648f42a073819da90a17ab495
edmarSoaress <edmar.slima@mercadolivre.com>
2023-08-25 15:24:57 -0300

    corrige file path

--- tests/MercadoPago/Client/CardToken/CardTokenClientUnitTest.php
+++ tests/MercadoPago/Client/CardToken/CardTokenClientUnitTest.php
@@ -32,7 +32,7 @@ final class CardTokenClientUnitTest extends TestCase
 
     public function testGetSuccess(): void
     {
-        $file_path = '../../../Resources/Mocks/Response/Cardtoken/card_token_get.json';
+        $file_path = '../../../Resources/Mocks/Response/CardToken/card_token_get.json';
         $mock_http_request = $this->mockHttpRequest($file_path, 200);
 
         $http_client = new MPDefaultHttpClient($mock_http_request);
