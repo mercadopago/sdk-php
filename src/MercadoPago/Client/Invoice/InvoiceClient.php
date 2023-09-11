4ff599419e427b9c2b319b757e32d2cfb2b56b50
rhames07 <rhames.lima@mercadolivre.com>
2023-09-11 14:34:28 -0300

    Fix InvoiceClient

--- src/MercadoPago/Client/Invoice/InvoiceClient.php
+++ src/MercadoPago/Client/Invoice/InvoiceClient.php
@@ -4,7 +4,6 @@ namespace MercadoPago\Client\Invoice;
 
 use MercadoPago\Client\MercadoPagoClient;
 use MercadoPago\Core\MPRequestOptions;
-use MercadoPago\Exceptions\MPApiException;
 use MercadoPago\MercadoPagoConfig;
 use MercadoPago\Net\HttpMethod;
 use MercadoPago\Net\MPSearchRequest;
@@ -30,17 +29,15 @@ final class InvoiceClient extends MercadoPagoClient
      * @param int $id invoice id.
      * @param mixed $request_options request options to be sent.
      * @return \MercadoPago\Resources\Invoice invoice found.
+     * @throws \MercadoPago\Exceptions\MPApiException if the request fails.
+     * @throws \Exception if the request fails.
      */
     public function get(int $id, ?MPRequestOptions $request_options = null): Invoice
     {
-        try {
-            $response = parent::send(sprintf(self::URL_WITH_ID, $id), HttpMethod::GET, null, null, $request_options);
-            $result = Serializer::deserializeFromJson(Invoice::class, $response->getContent());
-            $result->setResponse($response);
-            return $result;
-        } catch (MPApiException | \Exception $e) {
-            throw $e;
-        }
+        $response = parent::send(sprintf(self::URL_WITH_ID, $id), HttpMethod::GET, null, null, $request_options);
+        $result = Serializer::deserializeFromJson(Invoice::class, $response->getContent());
+        $result->setResponse($response);
+        return $result;
     }
 
     /**
@@ -48,17 +45,15 @@ final class InvoiceClient extends MercadoPagoClient
      * @param \MercadoPago\Net\MPSearchRequest $request search request.
      * @param mixed $request_options request options to be sent.
      * @return \MercadoPago\Resources\InvoiceSearch search results.
+     * @throws \MercadoPago\Exceptions\MPApiException if the request fails.
+     * @throws \Exception if the request fails.
      */
     public function search(MPSearchRequest $request, ?MPRequestOptions $request_options = null): InvoiceSearch
     {
-        try {
-            $query_params = isset($request) ? $request->getParameters() : null;
-            $response = parent::send(self::URL_SEARCH, HttpMethod::GET, null, $query_params, $request_options);
-            $result = Serializer::deserializeFromJson(InvoiceSearch::class, $response->getContent());
-            $result->setResponse($response);
-            return $result;
-        } catch (MPApiException | \Exception $e) {
-            throw $e;
-        }
+        $query_params = isset($request) ? $request->getParameters() : null;
+        $response = parent::send(self::URL_SEARCH, HttpMethod::GET, null, $query_params, $request_options);
+        $result = Serializer::deserializeFromJson(InvoiceSearch::class, $response->getContent());
+        $result->setResponse($response);
+        return $result;
     }
 }
