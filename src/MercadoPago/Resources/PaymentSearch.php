14864a69576d3dd4e18f7f798f620fd98d2462d0
rhames07 <rhames.lima@mercadolivre.com>
2023-09-26 10:42:38 -0300

    Map Payment search

--- src/MercadoPago/Resources/PaymentSearch.php
+++ src/MercadoPago/Resources/PaymentSearch.php
@@ -3,13 +3,30 @@
 namespace MercadoPago\Resources;
 
 use MercadoPago\Net\MPResource;
+use MercadoPago\Serialization\Mapper;
 
 /** PaymentSearch class. */
 class PaymentSearch extends MPResource
 {
+    /** Class mapper. */
+    use Mapper;
+
     /** Search paging. */
-    public array $paging;
+    public object|array|null $paging;
 
     /** Search results. */
-    public array $results;
+    public object|array|null $results;
+
+    private $map = [
+        "paging" => "MercadoPago\Resources\Common\Paging",
+        "results" => "MercadoPago\Resources\Payment\PaymentSearchResult",
+    ];
+
+    /**
+     * Method responsible for getting map of entities.
+     */
+    public function getMap(): array
+    {
+        return $this->map;
+    }
 }
