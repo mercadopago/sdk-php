ceccbcaa3b01d24d19a0ccc6bafdc02d07e541d0
meliguilhermefernandes <guilherme.jfernandes@mercadolivre.com>
2024-02-02 16:48:30 -0300

    Add x-idempotency-key header to requests

--- src/MercadoPago/Manager.php
+++ src/MercadoPago/Manager.php
@@ -84,7 +84,7 @@ class Manager
 
         $this->_setDefaultHeaders($configuration->query);
         $this->_setCustomHeaders($entity, $configuration->query);
-        //$this->_setIdempotencyHeader($configuration->query, $configuration, $method);
+        $this->_setIdempotencyHeader($configuration->query, $method);
         $this->setQueryParams($entity);
         
         return $this->_client->{$method}($configuration->url, $configuration->query);
@@ -409,17 +409,12 @@ class Manager
      * @param        $configuration
      * @param string $method
      */
-    protected function _setIdempotencyHeader(&$query, $configuration, $method)
+    protected function _setIdempotencyHeader(&$query, $method)
     {
-        if (!isset($configuration->methods[$method])) {
-            return;
-        }
-        $fields = '';
-        if ($configuration->methods[$method]['idempotency']) {
-            $fields = $this->_getIdempotencyAttributes($configuration->attributes);
-        }
-        if ($fields != '') {
-            $query['headers']['x-idempotency-key'] = hash(self::$CIPHER, $fields);
+        if ($method != 'get' && $method != 'delete') {
+            if (array_key_exists('headers', array_change_key_case($query)) && !array_key_exists('x-idempotency-key', array_change_key_case($query['headers']))){
+                $query['headers']['x-idempotency-key'] = $this->_generateUUID();
+            }
         }
     }
     /**
@@ -427,14 +422,30 @@ class Manager
      *
      * @return string
      */
-    protected function _getIdempotencyAttributes($attributes)
+    protected function _generateUUID()
     {
-        $result = [];
-        foreach ($attributes as $key => $value) {
-            if ($value['idempotency']) {
-                $result[] = $key;
-            }
-        }
-        return implode('&', $result);
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+
+            // 16 bits for "time_mid"
+            mt_rand(0, 0xffff),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand(0, 0x0fff) | 0x4000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand(0, 0x3fff) | 0x8000,
+
+            // 48 bits for "node"
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
+        );
     }
 }
