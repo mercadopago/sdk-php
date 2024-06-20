b29a126f458877eecf33bcd22b6901adae90e94c
Leonardo Buares <contact@leonardobuares.dev>
2024-06-20 14:48:00 -0300

    Improve UUID generation method (#514)

--- src/MercadoPago/Client/MercadoPagoClient.php
+++ src/MercadoPago/Client/MercadoPagoClient.php
@@ -152,30 +152,13 @@ class MercadoPagoClient
     }
 
 
-    private function generateUUID()
+    private function generateUUID(): string
     {
-        return sprintf(
-            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
+        $data = random_bytes(16);
 
-            // 16 bits for "time_mid"
-            mt_rand(0, 0xffff),
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
 
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand(0, 0x0fff) | 0x4000,
-
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff) | 0x8000,
-
-            // 48 bits for "node"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
-        );
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 }
