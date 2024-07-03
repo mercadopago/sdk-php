8ec39afc5210f860ac0e81fc779090e0a4f6d112
Danielen Cestari Nunes <danielen.cestari@mercadolivre.com>
2024-07-03 15:40:57 -0300

    fix address_door_number

--- src/MercadoPago/Resources/Common/SubMerchant.php
+++ src/MercadoPago/Resources/Common/SubMerchant.php
@@ -15,7 +15,7 @@ class SubMerchant
     public ?string $country;
 
     /** Street number where the submerchant is located. */
-    public ?string $address_door_number;
+    public ?number $address_door_number;
 
     /**  CEP of the submerchant. */
     public ?string $zip;
