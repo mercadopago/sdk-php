7074144d26de01f0fc8ab20ac8bfb6e7767b9732
andreagostinho-meli <andre.agostinho@mercadolivre.com>
2024-11-25 17:22:41 -0300

    feat: add update transaction method

--- src/MercadoPago/Client/Order/OrderTransactionClient.php
+++ src/MercadoPago/Client/Order/OrderTransactionClient.php
@@ -6,6 +6,7 @@ use MercadoPago\Client\Common\RequestOptions;
 use MercadoPago\Client\MercadoPagoClient;
 use MercadoPago\Client\Order\Transaction\CreateTransactionRequest;
 use MercadoPago\Client\Order\Transaction\TransactionResponse;
+use MercadoPago\Client\Order\Transaction\UpdateTransaction;
 use MercadoPago\MercadoPagoConfig;
 use MercadoPago\Net\HttpMethod;
 use MercadoPago\Net\MPHttpClient;
@@ -15,6 +16,7 @@ use MercadoPago\Serialization\Serializer;
 final class OrderTransactionClient extends MercadoPagoClient
 {
     private const URL = "/v1/orders/%s/transactions";
+    private const URL_WITH_ID = "/v1/orders/%s/transactions/%s";
 
     /** Default constructor. Uses the default http client used by the SDK or custom http client provided. */
     public function __construct(?MPHttpClient $MPHttpClient = null)
@@ -36,6 +38,25 @@ final class OrderTransactionClient extends MercadoPagoClient
         $path = sprintf(self::URL, $order_id);
         $response = parent::send($path, HttpMethod::POST, json_encode($request), null, $request_options);
         $result = Serializer::deserializeFromJson(TransactionResponse::class, $response->getContent());
+        $result->setResponse($response);
+        return $result;
+    }
+
+    /**
+     * Method responsible for updating an Order transaction.
+     * @param string $order_id Order ID.
+     * @param string $transaction_id Transaction ID.
+     * @param \MercadoPago\Client\Order\UpdateTransaction $request Update Transaction request.
+     * @param \MercadoPago\Client\Common\RequestOptions request options to be sent.
+     * @return \MercadoPago\Client\Order\UpdateTransaction Transaction updated.
+     * @throws \MercadoPago\Exceptions\MPApiException if the request fails.
+     * @throws \Exception if the request fails.
+     */
+    public function update(string $order_id, string $transaction_id, UpdateTransaction $request, ?RequestOptions $request_options = null): UpdateTransaction
+    {
+        $path = sprintf(self::URL_WITH_ID, $order_id, $transaction_id);
+        $response = parent::send($path, HttpMethod::PATCH, json_encode($request), null, $request_options);
+        $result = Serializer::deserializeFromJson(UpdateTransaction::class, $response->getContent());
         $result->setResponse($response);
         return $result;
     }
