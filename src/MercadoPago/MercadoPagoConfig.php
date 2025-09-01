6105506d9734285622ad34d46a02cf5f3f8ac0cb
rsosantos <anderson.rsantos@mercadolivre.com>
2025-09-01 09:36:23 -0300

    chore(release): bump SDK to 3.6.0; update README; add CHANGELOG 3.6.0

--- src/MercadoPago/MercadoPagoConfig.php
+++ src/MercadoPago/MercadoPagoConfig.php
@@ -10,7 +10,7 @@ use MercadoPago\Net\MPHttpClient;
 class MercadoPagoConfig
 {
     /** @var string Mercado Pago SDK version. */
-    public static string $CURRENT_VERSION = "3.5.1";
+    public static string $CURRENT_VERSION = "3.6.0";
   
     /** @var string Mercado Pago Base URL */
     public static string $BASE_URL = "https://api.mercadopago.com";
