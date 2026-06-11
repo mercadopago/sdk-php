dd78318a02d96d6af6c123a1124b0f82a335bd67
Luis Sanchez <luismiguel.sanchezgarcia@mercadolibre.com.co>
2026-06-10 20:17:50 -0500

    Bump version

--- src/MercadoPago/MercadoPagoConfig.php
+++ src/MercadoPago/MercadoPagoConfig.php
@@ -22,7 +22,7 @@ use MercadoPago\Net\MPHttpClient;
 class MercadoPagoConfig
 {
     /** @var string Mercado Pago SDK version. */
-    public static string $CURRENT_VERSION = "3.11.1";
+    public static string $CURRENT_VERSION = "3.11.2";
 
     /** @var string Base URL for all API requests. Override only for testing or proxy scenarios. */
     public static string $BASE_URL = "https://api.mercadopago.com";
